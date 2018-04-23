1e1de6bbab0fc4f6b6014d1a989e936be76fc9d8
David Blood <davidbjourno@users.noreply.github.com>
2018-04-23 13:25:45 +0100

    Update Mapbox style; add navigation controls

--- client/components/app/index.js
+++ client/components/app/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { NavigationControl } from 'react-map-gl';
 import './styles.scss';
 
-const MAPBOX_STYLE = 'mapbox://styles/mapbox/dark-v9';
+const MAPBOX_STYLE = 'mapbox://styles/financialtimes/cjg290kic7od82rn46o3o719e';
 const MAPBOX_TOKEN = window.mapboxToken;
 
 class App extends Component {
@@ -13,10 +13,11 @@ class App extends Component {
       viewport: {
         width: window.innerWidth,
         height: window.innerHeight,
-        longitude: -74,
-        latitude: 40.7,
-        zoom: 11,
-        maxZoom: 16,
+        longitude: -2.5,
+        latitude: 54.5,
+        zoom: 5,
+        maxZoom: 10,
+        minZoom: 5,
       },
     };
     this.resize = this.resize.bind(this);
@@ -49,19 +50,26 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <ReactMapGL
-          {...this.state.viewport}
-          mapStyle={MAPBOX_STYLE}
-          mapboxApiAccessToken={MAPBOX_TOKEN}
-          onViewportChange={viewport => this.onViewportChange(viewport)}
-          scrollZoom={false}
-          dragPan={false}
-          dragRotate={false}
-          doubleClickZoom={false}
-          touchZoom={false}
-        />
-      </div>
+      <ReactMapGL
+        {...this.state.viewport}
+        mapStyle={MAPBOX_STYLE}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+        onViewportChange={viewport => this.onViewportChange(viewport)}
+        scrollZoom={false}
+        dragRotate={false}
+        doubleClickZoom={false}
+        touchZoom={false}
+      >
+        <div className="navigation-control-container">
+          <NavigationControl
+            onViewportChange={(viewport) => {
+              const { maxZoom, minZoom, ...viewportNoMaxMin } = viewport;
+
+              return this.onViewportChange(viewportNoMaxMin);
+            }}
+          />
+        </div>
+      </ReactMapGL>
     );
   }
 }
