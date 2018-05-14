c9555b187fcfedaa2f2237e3a0115be4c80155ef
David Blood <davidbjourno@users.noreply.github.com>
2018-05-14 21:50:19 +0100

    Add basic viewport transition functionality and form component

--- client/components/app/index.js
+++ client/components/app/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import ReactMapGL, { NavigationControl } from 'react-map-gl';
+import ReactMapGL, { NavigationControl, FlyToInterpolator } from 'react-map-gl';
+import * as d3 from 'd3-ease'; // eslint-disable-line
+import GeographyLookup from './geography-lookup';
 import './styles.scss';
 
 const MAPBOX_STYLE = 'mapbox://styles/financialtimes/cjg290kic7od82rn46o3o719e';
@@ -21,6 +23,9 @@ class App extends Component {
       },
     };
     this.resize = this.resize.bind(this);
+    this.goToViewport = this.goToViewport.bind(this);
+    this.handleChange = this.handleGeographyChange.bind(this);
+    this.handleGeographySubmit = this.handleGeographySubmit.bind(this);
   }
 
   componentDidMount() {
@@ -48,28 +53,61 @@ class App extends Component {
     });
   }
 
+  goToViewport() {
+    const nextLocation = { longitude: -0.070691, latitude: 51.4594016, zoom: 10 };
+    const { longitude, latitude, zoom } = nextLocation;
+
+    this.onViewportChange({
+      longitude,
+      latitude,
+      zoom,
+      transitionDuration: 5000,
+      transitionInterpolator: new FlyToInterpolator(),
+      transitionEasing: d3.easeCubic,
+    });
+  }
+
+  handleGeographyChange(geography) {
+    console.log(`Typing: ${geography}…`);
+  }
+
+  handleGeographySubmit(geography) {
+    console.log(`Submitted: ${geography}`);
+
+    this.setState({ activeGeography: geography });
+  }
+
   render() {
     return (
-      <ReactMapGL
-        {...this.state.viewport}
-        mapStyle={MAPBOX_STYLE}
-        mapboxApiAccessToken={MAPBOX_TOKEN}
-        onViewportChange={viewport => this.onViewportChange(viewport)}
-        scrollZoom={false}
-        dragRotate={false}
-        doubleClickZoom={false}
-        touchZoom={false}
-      >
-        <div className="navigation-control-container">
-          <NavigationControl
-            onViewportChange={(viewport) => {
-              const { maxZoom, minZoom, ...viewportNoMaxMin } = viewport;
+      <div>
+        <button onClick={this.goToViewport}>Start transition</button>
 
-              return this.onViewportChange(viewportNoMaxMin);
-            }}
-          />
-        </div>
-      </ReactMapGL>
+        <GeographyLookup
+          onGeographyChange={this.handleGeographyChange}
+          onGeographySubmit={this.handleGeographySubmit}
+        />
+
+        <ReactMapGL
+          {...this.state.viewport}
+          mapStyle={MAPBOX_STYLE}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          onViewportChange={viewport => this.onViewportChange(viewport)}
+          scrollZoom={false}
+          dragRotate={false}
+          doubleClickZoom={false}
+          touchZoom={false}
+        >
+          <div className="navigation-control-container">
+            <NavigationControl
+              onViewportChange={(viewport) => {
+                const { maxZoom, minZoom, ...viewportNoMaxMin } = viewport;
+
+                return this.onViewportChange(viewportNoMaxMin);
+              }}
+            />
+          </div>
+        </ReactMapGL>
+      </div>
     );
   }
 }
