eebcf00348bc9c5dc6c70f94efaf957628b7d3bc
Ændrew Rininsland <andrew.rininsland@ft.com>
2018-05-29 14:49:52 +0100

    Adds ability to manage copy via Google Docs

--- config/sanitise.js
+++ config/sanitise.js
@@ -5,7 +5,8 @@ export default async function sanitizeDoc(html) {
 
   return $('body')
     .children()
-    .map((idx, el) => $.html(el))
-    .toArray()
-    .filter(e => e !== '<p></p>');
+    .map((idx, el) => $.html(el)) // N.b., this is Cheerio's bizarro jQuery-esque .map()
+    .toArray() // Now it's an actual Array-like.
+    .filter(e => e !== '<p></p>') // Remove empty paragraphs
+    .map(el => el.replace(/^<p>(.+?)<\/p>$/, '$1')); // Remove wrapping p tags
 }
