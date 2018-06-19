ed99657bb5eb90d7323c2449458e29208f3050bb
Ændrew Rininsland <andrew.rininsland@ft.com>
2018-06-19 11:38:35 +0100

    Tidying up condition as per @maxharlow's suggestion

--- client/components/app/histogram/index.js
+++ client/components/app/histogram/index.js
@@ -181,7 +181,7 @@ export default class Histogram extends React.Component {
         '#ffdca2',
         '#ffffcc',
       ])(value / bins.length);
-    if (this.props.geography && Object.keys(this.props.geography).length > 0 && region) {
+    if (region) {
       svg
         .append('g')
         .selectAll()
